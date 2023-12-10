ffb04001e7dbc9d21100face9037f6c959043529
Naomi Mogi <nyabokenaomi001@gmail.com>
2023-12-10 17:11:31 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+const mainElement = document.getElementById("main");
+
+if (mainElement) {
+  mainElement.remove();
+} else {
+  console.warn("Main element not found!");
+}
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+const myName = "Naomi";
+newHeader.textContent = `${myName} is the champion`;
+document.body.appendChild(newHeader);
